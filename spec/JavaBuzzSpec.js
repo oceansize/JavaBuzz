f6a9f5f71a90c1f4b3c79cd2a2513beb69c3630a
Roi Driscoll <roi.driscoll@gmail.com>
2014-09-26 07:48:16 +0100

    3,5, and 15 methods implemented - all tests(refactored) pass.

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -12,18 +12,30 @@ describe("Javabuzz", function() {
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
 
-    it('not divisible by 3', function(){
-      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
-    });
-
     it('divisible by 5', function(){
       expect(javabuzz.isDivisibleByFive(5)).toBe(true);
     });
 
-    it('not divisible by 5', function(){
+    it('divisible by 15', function(){
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+
+
+  }); // Close describe
+
+  describe('knows when a number is not', function(){
+
+    it('divisible by 3', function(){
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it('divisible by 5', function(){
       expect(javabuzz.isDivisibleByFive(1)).toBe(false);
     });
 
-  }); // Close describe
+    it('divisible by 15', function(){
+      expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
+    });
+  });
 
 }); // Close describe
--- src/JavaBuzz.js
+++ src/JavaBuzz.js
@@ -7,3 +7,7 @@ Javabuzz.prototype.isDivisibleByThree = function(number){
 Javabuzz.prototype.isDivisibleByFive = function(number){
   return number % 5 === 0;
 }
+
+Javabuzz.prototype.isDivisibleByFifteen = function(number){
+  return number % 15 === 0;
+}
